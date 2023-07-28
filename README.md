de3527aaa4e0548ca4f4328a182c3c753b875893
Thibaud Brault <thibaud.brault@protonmail.com>
2023-07-28 15:23:35 +0200

    :tada: init project

---
+++ README.md
@@ -0,0 +1,3 @@
+# `unit-converter`
+
+Package with multiple functions to convert units of measurements
---
+++ index.ts
@@ -0,0 +1,32 @@
+export const feetToMeter = (unit: number | string, fixed: number = 2) => {
+  let formattedUnit = unit;
+  if (typeof formattedUnit === "string" && formattedUnit.includes(",")) {
+    formattedUnit = formattedUnit.replace(",", ".");
+  }
+  if (typeof formattedUnit === "number") {
+    formattedUnit = formattedUnit.toString();
+  }
+  let [feet = 0, inches = 0] = formattedUnit.split(".");
+  if (typeof feet === "string") {
+    feet = parseFloat(feet);
+  }
+  if (typeof inches === "string") {
+    inches = parseFloat(inches);
+  }
+  const result = feet * 0.3048 + inches * 0.0254;
+  return result.toFixed(fixed);
+};
+
+export const meterToFeet = (unit: number | string, fixed: number = 2) => {
+  let formattedUnit = unit;
+  if (typeof formattedUnit === "string") {
+    formattedUnit = formattedUnit.includes(",")
+      ? formattedUnit.replace(",", ".")
+      : formattedUnit;
+    formattedUnit = parseFloat(formattedUnit);
+  }
+  const length = formattedUnit / 0.0254;
+  const feet = Math.floor(length / 12);
+  const inches = Number((length - 12 * feet).toFixed(fixed));
+  return { feet, inches };
+};
